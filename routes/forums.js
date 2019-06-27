ab06a09ec1b18ad17c9273bb0ad053d072f04625
Isabel <isrodriguezruiz@gmail.com>
2019-06-27 13:52:38 +0200

    BBDD

--- routes/forums.js
+++ routes/forums.js
@@ -9,6 +9,8 @@ exports.getForums = (req, res) =>{
     
     var url = req.body.url + '/webservice/rest/server.php?wsfunction=mod_forum_get_forums_by_courses&courseids[0]=' + req.body.id + '&moodlewsrestformat=json';
     var courseid = req.body.id;
+    var arrayForums = [];
+    var arrayWikis = [];
     var options = {
             url: url,
             method: 'GET',
@@ -17,11 +19,10 @@ exports.getForums = (req, res) =>{
                 'wstoken': req.body.token
             }
         }
-        // Start the request
-    request(options, function(error, response, body) {
+    try{
+    request(options, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             // Print out the response body
-            var arrayForums = [];
             var response = JSON.parse(body);
             for (let forum of response) {
                 arrayForums.push({
@@ -30,7 +31,34 @@ exports.getForums = (req, res) =>{
                     courseid:courseid,
                 });
             }
-            res.render('forums', {token:req.body.token, courseid:courseid, url:req.body.url,array:arrayForums});
+            //res.render('forums', {token:req.body.token, courseid:courseid, url:req.body.url,array:arrayForums, arrayW:arrayWikis});
+            var optionsWiki = {
+                url: req.body.url + '/webservice/rest/server.php?wsfunction=mod_wiki_get_wikis_by_courses&courseids[0]='+ courseid +'&moodlewsrestformat=json',
+                method: 'GET',
+                headers: headers,
+                qs: {
+                    'wstoken': req.body.token,
+                    }
+                }
+                console.log(optionsWiki)
+                request(optionsWiki, (error, response, body) =>{
+                    if (!error && response.statusCode == 200) {
+                        var responseWiki = JSON.parse(body);
+                        console.log(responseWiki)
+                        for (let wiki of responseWiki.wikis) {
+                            arrayWikis.push({
+                            wikiid: wiki.id,
+                            wikiname: wiki.name,
+                            courseid:courseid,
+                        });
+                        }
+                    res.render('forums', {token:req.body.token, courseid:courseid, url:req.body.url,array:arrayForums, arrayW:arrayWikis});    
+                    }
+                });
         }
     });
+}catch(error){
+    throw error;
 }
+}
+//http://maquetagephi.tige.ior.etsit.upm.es/webservice/rest/server.php?wstoken=0d7d74e9370202ceecc09d63d97f5f40&wsfunction=mod_wiki_get_wikis_by_courses&courseids[0]=2&moodlewsrestformat=json
